920f60f42b53f9cceb38de33e020b575dcef5d08
a <ahmed-adm@lm.local>
2020-09-01 14:28:19 +0900

    Removed all Hardcoded Paths (as per chocolatey teams guidance. Thank you very much).

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 # Package Variables
 $packageName= 'jwpce'
 $shortcutName= 'JWPce'
-$toolsDir   = "C:\Program Files\$shortcutName"
+$toolsDir   = "$env:ProgramFiles\$shortcutName"
 $url_jwpce        = 'https://mijet.eludevisibility.org/JWPxp/JWPce%201.50.7z' # Permalink (from M.I.J.E.T's Webpage).
 $packageArgs = @{
   packageName   = $packageName
@@ -19,6 +19,6 @@ Stop-Process  -Name "$packageName" -force 2>$null
 Install-ChocolateyZipPackage @packageArgs 
 
 # Install shortcuts in the Windows Start Menu and Desktop
-mkdir "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$packageName"
-Install-ChocolateyShortcut -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" -TargetPath "$toolsDir\$packageName.exe"
-Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" "C:\Users\Public\Desktop"
+mkdir "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$packageName"
+Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" -TargetPath "$toolsDir\$packageName.exe"
+Copy-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" "$env:ProgramData\Desktop"
--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -2,8 +2,8 @@
 $ErrorActionPreference = 'SilentlyContinue';
 Stop-Process  -Name 'jwpce' -force 2>$null
 Start-Sleep -Seconds 1 2>$null
-remove-item -Recurse -Force "$Env:ProgramFiles\JWPce" 2>$null
-remove-item -Recurse -Force "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Programs\jwpce" 2>$null
-remove-item -Recurse -Force "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Programs\JWPce.lnk" 2>$null
-remove-item -Recurse -Force "$Env:PUBLIC\Desktop\JWPce.lnk" 2>$null
+remove-item -Recurse -Force "$env:ProgramFiles\JWPce" 2>$null
+remove-item -Recurse -Force "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Programs\jwpce" 2>$null
+remove-item -Recurse -Force "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Programs\JWPce.lnk" 2>$null
+remove-item -Recurse -Force "$env:PUBLIC\Desktop\JWPce.lnk" 2>$null
 $ErrorActionPreference = 'Stop'; # stop on all errors
