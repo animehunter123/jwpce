a3fdc66166f0e9b9112502dd9ae04909041233bc
a <ahmed-adm@lm.local>
2020-09-01 14:24:55 +0900

    Removed Hard Coded Path Names

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,10 +1,9 @@
 #Remove the JWPce Program Files, Desktop Shortcut, and Start Menu shortcut
 $ErrorActionPreference = 'SilentlyContinue';
-$toolsDir   = 'C:\Program Files\JWPce'
 Stop-Process  -Name 'jwpce' -force 2>$null
 Start-Sleep -Seconds 1 2>$null
-remove-item -Recurse -Force $toolsDir 2>$null
-remove-item -Recurse -Force 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\jwpce' 2>$null
-remove-item -Recurse -Force "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JWPce.lnk" 2>$null
-remove-item -Recurse -Force "C:\Users\Public\Desktop\JWPce.lnk" 2>$null
+remove-item -Recurse -Force "$Env:ProgramFiles\JWPce" 2>$null
+remove-item -Recurse -Force "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Programs\jwpce" 2>$null
+remove-item -Recurse -Force "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Programs\JWPce.lnk" 2>$null
+remove-item -Recurse -Force "$Env:PUBLIC\Desktop\JWPce.lnk" 2>$null
 $ErrorActionPreference = 'Stop'; # stop on all errors
