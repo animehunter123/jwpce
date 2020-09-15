3896389c6dd34c550a35c866be6214dde1ff11d0
a <ahmed-adm@lm.local>
2020-09-15 12:37:01 +0900

    Fixed the typo for double quotes in uninstaller

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = 'SilentlyContinue';
 $packageName= 'jwpce'
 $shortcutName= 'JWPce'
-Stop-Process  -Name '$packageName' -force 2>$null
+Stop-Process  -Name "$packageName" -force 2>$null
 Start-Sleep -Seconds 1 2>$null
 remove-item -Recurse -Force "$env:ProgramFiles\$shortcutName" 2>$null
 remove-item -Recurse -Force "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$packageName" 2>$null
